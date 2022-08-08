a5ab98bb6e6741e7e4dfa6e4ab0a6acab137e15c
AlanJoji <alanjojiveliyath@yahoo.com>
2022-08-08 16:37:14 +0530

    Image Folder Empty Fix

--- recognition_image_load.py
+++ recognition_image_load.py
@@ -2,22 +2,26 @@ import os
 import face_recognition
 
 def img_load() :
-
-    if not os.path.exists('Images'):
-        os.makedirs('Images')
     DIR = r'Images'
+
+    if not os.path.exists(DIR):
+        os.makedirs(DIR)
 
     known_face_names = []
     known_face_encodings = []
 
-    for image_file in os.listdir(DIR) :
-        image_path = os.path.join(DIR, image_file)
-        #print(image_path)
+    no_of_files = len(os.listdir(DIR))
 
-        image = face_recognition.load_image_file(image_path)
-        encoding = face_recognition.face_encodings(image)[0]
+    if (no_of_files > 0) :
+        for image_file in os.listdir(DIR) :
+            image_path = os.path.join(DIR, image_file)
 
-        known_face_names.append(image_file[:-4])
-        known_face_encodings.append(encoding)
+            image = face_recognition.load_image_file(image_path)
+            encoding = face_recognition.face_encodings(image)[0]
+
+            known_face_names.append(image_file[:-4])
+            known_face_encodings.append(encoding)
+    else :
+        quit()
 
     return(known_face_names, known_face_encodings)
